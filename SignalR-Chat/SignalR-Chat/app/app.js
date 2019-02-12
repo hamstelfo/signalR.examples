58f3e07e777f020f1aa99f77f5e391f7a7539890
Rubén Peinado <rpeinado@asystem.es>
2019-02-12 15:09:42 +0100

    SignalR first adaption
    Signed-off-by: Rubén Peinado <rpeinado@asystem.es>

--- SignalR-Chat/SignalR-Chat/app/app.js
+++ SignalR-Chat/SignalR-Chat/app/app.js
@@ -18,6 +18,15 @@
         $('#messagesArea').append('<li><strong>' + username + '</strong>: ' + message);
     }
 
+    chat.client.showError = function (message) {
+        $('#theError').html(message);
+        $('#theError').show();
+    }
+    chat.client.showOK = function (message) {
+        $('#theSuccess').html(message);
+        $('#theSuccess').show();
+    }
+
     // En la conexión enviamos también el usuario, aunque podríamos haber hecho esto:
     //$.connection.hub.start();
     $.connection.hub.start().done(function () {
@@ -28,6 +37,22 @@
         var message = $('#userMessage').val();
         chat.server.send(message);
         $('#userMessage').val("");
+    });
+
+    $('#btnSendMessageJSon').click(function () {
+        var message = prepareMessage(6, $('#userMessage2').val());
+        chat.server.sendJSon(message);
+        $('#userMessage2').val("");
+        $('#theSuccess').hide();
+        $('#theError').hide();
+    });
+
+    $('#btnSendMessageJSonError').click(function () {
+        var message = $('#userMessage2').val();
+        chat.server.sendJSon(message);
+        $('#userMessage2').val("");
+        $('#theSuccess').hide();
+        $('#theError').hide();
     });
 
 });
---
+++ SignalR-Chat/SignalR-Chat/app/com.js
@@ -0,0 +1,10 @@
+﻿
+
+function prepareMessage(type, data) {
+    var message = {
+        type: type,
+        data: data
+    };
+
+    return JSON.stringify(message);
+}
